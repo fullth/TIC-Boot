dc92d3db10dfb22fd64328866af4b3c90dd68205
fullth <47442733+fullth@users.noreply.github.com>
2021-04-16 16:26:55 +0900

    Add parse logic in Naver Search API

--- TIC-boot/src/main/java/com/fullth/tic/controller/MotorTrendController.java
+++ TIC-boot/src/main/java/com/fullth/tic/controller/MotorTrendController.java
@@ -1,5 +1,8 @@
 package com.fullth.tic.controller;
 
+import java.util.List;
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,15 +11,18 @@ import com.fullth.tic.service.MotorTrendService;
 
 @Controller
 public class MotorTrendController {
-	
+
 	@Autowired
 	MotorTrendService motorTrendService;
-	
+
 	@GetMapping("/Motor")
 	public String motorTrend() {
+
+		Map<String, Object> reqTesla = motorTrendService.getMotorTrend("KEYWORD");
+		List<Map<String, Object>> items = (List<Map<String, Object>>) reqTesla.get("result");
 		
-		// API test.
-		motorTrendService.getMotorTrend();
+		for(int i = 0; i < items.size(); i++ )
+			System.out.println(items.get(i).get("title"));
 		
 		return "MotorTrend";
 	}
--- TIC-boot/src/main/java/com/fullth/tic/service/MotorTrendService.java
+++ TIC-boot/src/main/java/com/fullth/tic/service/MotorTrendService.java
@@ -5,58 +5,89 @@ import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.net.URLEncoder;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class MotorTrendService {
-	
+
 	@Autowired
 	PropReader propReader;
-	
-	public void getMotorTrend() {
-		
+
+	/**
+	 * 
+	 * @param keyword
+	 * @return rtnObj
+	 */
+	public Map<String, Object> getMotorTrend(String keyword) {
+
 		List<String> propList = propReader.propRead();
-		
-        String clientId = propList.get(0);
+
+		String clientId = propList.get(0);
 		String clientSecret = propList.get(1);
 		String apiURL = propList.get(2);
-		
-		System.out.println("clientId:" + clientId + ", clientSecret:" + clientSecret + ", apiURL:" + apiURL);
-				
-        try {
-            String keyword = URLEncoder.encode("KEYWORD", "UTF-8");
-            apiURL += "?query=" + keyword;
-            URL url = new URL(apiURL);
-            
-            HttpURLConnection con = (HttpURLConnection)url.openConnection();
-            
-            con.setRequestMethod("GET");
-            con.setRequestProperty("X-Naver-Client-Id", clientId);
-            con.setRequestProperty("X-Naver-Client-Secret", clientSecret);
-            
-            int responseCode = con.getResponseCode();
-            
-            BufferedReader br;
-            
-            if(responseCode == 200) { 
-                br = new BufferedReader(new InputStreamReader(con.getInputStream()));
-            } else { 
-                br = new BufferedReader(new InputStreamReader(con.getErrorStream()));
-            }
-            
-			String parseResponse;
-			StringBuffer response = new StringBuffer();
-			
+
+		StringBuffer response = new StringBuffer();
+		String parseResponse = null;
+
+		Map<String, Object> rtnObj = new HashMap<>();
+
+		try {
+			String search = URLEncoder.encode(keyword, "UTF-8");
+			apiURL += "?query=" + search;
+			URL url = new URL(apiURL);
+
+			HttpURLConnection con = (HttpURLConnection) url.openConnection();
+
+			con.setRequestMethod("GET");
+			con.setRequestProperty("X-Naver-Client-Id", clientId);
+			con.setRequestProperty("X-Naver-Client-Secret", clientSecret);
+
+			int responseCode = con.getResponseCode();
+
+			BufferedReader br;
+
+			if (responseCode == 200) {
+				br = new BufferedReader(new InputStreamReader(con.getInputStream()));
+			} else {
+				br = new BufferedReader(new InputStreamReader(con.getErrorStream()));
+			}
+
 			while ((parseResponse = br.readLine()) != null) {
 				response.append(parseResponse);
-				System.out.println(parseResponse);
 			}
-			// TODO Method refactoring.		
-        } catch (Exception e) {
-            System.out.println(e);
-        }
-    }
+
+			System.out.println(response.toString());
+
+			JSONParser parser = new JSONParser();
+			JSONObject result = (JSONObject) parser.parse(response.toString());
+
+			JSONArray items = (JSONArray) result.get("items");
+			List<Map<String, Object>> itemList = new ArrayList<>();
+
+			String[] fields = { "title", "link", "description" };
+
+			for (int i = 0; i < items.size(); i++) {
+				JSONObject item = (JSONObject) items.get(i);
+				Map<String, Object> itemMap = new HashMap<>();
+				for (String field : fields) {
+					itemMap.put(field, item.get(field));
+				}
+				itemList.add(itemMap);
+			}
+			rtnObj.put("result", itemList);
+		} catch (Exception e) {
+			System.out.println(e);
+		}
+		return rtnObj;
+	}
 }
+// TODO Method refactoring.
